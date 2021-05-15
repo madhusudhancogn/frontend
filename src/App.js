e53d5980c7e066e56002408532c70a5eb09d8818
madhu <madhusudhan.g47@gmail.com>
2021-05-15 18:18:35 -0500

    added world

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello <code>Welcome</code> This is Madhu.
+          Hello <code>World</code> This is Madhu.
         </p>
         <a
           className="App-link"
